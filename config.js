ee3eeb8bcfb68befabde10621d5faece9d87f72e
GOLIATH130 <70407233+GOLIATH130@users.noreply.github.com>
2021-04-05 01:14:28 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,162 @@
+const ownerNumber = ["573175852207@s.whatsapp.net"] 
+// mude somente o numero e não tire o @s.whatsapp.net
+
+const OriginalOwner = '573175852207' 
+// mude para seu numero
+
+const apikeyporn = 'fc8fe174f018b95cdae9272493f1a12f'
+// sua key do imgbb para ativar o antiporn
+
+const cr = '𝙎𝘼𝙈𝙐𝙍𝘼𝙄 𝐁𝐎𝐓\n' 
+// texto do simbolo de verificado
+
+const prefix = '!' 
+// prefixo
+
+const blockedmsg = '*😜Tu número está bloqueado, es decir, no puedo escucharte 😜*'
+// mensagem quando alguem bloqueado solicita um comando
+
+const blockedcmdmsg = '*🚫Este comando se bloquea póngase en contacto con el propietario del bot para conocer el pq🚫*'
+// mensagem quando alguem solicita um comando bloqueado 
+
+// MENSAGENS DE BAN
+
+const banmsgtype = '*Ejecutar miembro común, la prohibición estar cerca*'
+// mensagem de ban no anti tipos de mensagem
+
+const adminmsgtype = '*Eres adm ent n Te prohibiré por enviar algún tipo de mensaje prohibido*'
+// mensagem de quando adm manda tipos de mensagens proibidas
+
+const banmsgporn = 'Hmmm con antiporn activado, ya saben bien...'
+// mensagem de ban no antiporn
+
+const adminmsgporn = '*Eres adm ent n Voy a prohibir ish que por enviar porno*'
+// mensagem de quando adm manda porn com antiporn ativado
+
+const banmsglink = 'Miembro común de Run, La prohibición está cerca...'
+// mensagem de ban no antilink
+
+const adminmsglink = 'Ese es un enlace de amigo... ah es que eres adm ent can kkkk 🙃'
+// mensagem de quando adm manda link
+
+const adminmsgpalavra = 'esto está en la lista de palabras prohibidas por el bot'
+// mensagem de quando adm manda uma palavra proibida pelo bot
+
+const banmsgpalavra = 'Palabra prohibida? Ya lo sabes, ¿verdad'
+// mensagem de ban quando manda uma palavra proibida pelo bot
+
+const qnttravahardroleta = 2
+// quantidade de travas que mandam no roleta russa hard
+// Obs: O valor é multiplicado por 12 ent tenha cuidado
+
+// MENSAGENS DE BOM DIA BOA TARDE E BOA NOITE
+
+const linkimgday = 'https://i.imgur.com/RR18JiI.jpg'
+const textmsgday = 'Ohayo gozaimasu, Onii-chan 👉👈'
+// link e mensagem de bom dia
+
+const linkimgeve = 'https://i.ibb.co/bLXgcBW/lp-tarde-anime-by-hitsukinyan-db7fpqr-fullview.jpg'
+const textmsgeve = `Kon'nichiwa Onii-chan, aceite esse café ☕`
+// link e mensagem de boa tarde
+
+const linkimgnig = 'https://i.ibb.co/VJCd34F/EKvo372-X0-AEd-Hz.jpg'
+const textmsgnig = 'Konbanwa Onii-chan, espero que tenha tido um bom dia ☺️'
+// link e mensagem de boa noite 
+
+// MENSAGENS DE ERRO
+
+const msgerr = 'Error intenta al rato:/'
+// mensagem de erro
+
+// MENSAGENS DE AUTOREPLY
+
+const botlindo = 'Bakaaaa 😣😣'
+const linkbotlindo = 'https://photos1.iorbix.com/00/00/00/00/02/72/43/64/C--6tZwaASQH-b.jpg'
+const botfeio = 'Pareces un sirgueijo, un cangrejo arrugado 😡'
+const cadebot = 'Me llamó onii-chan 👉👈?'
+const botfdp = 'Jódete, patéte el trasero cuando estés dormido'
+const botgostoso = 'Arigato go sai masu 😳👉👈'
+const botfofo = 'Arigato go sai masu 😳👉👈'
+const botbaianor = 'Invité a tu placa base a mi red, cuerno'
+const botcorno = 'Te follará, ff jugador'
+const botputa = 'tu madre 😡'
+const botgay = 'JAJA GAY EL HIJO QUE TENDRAS😡'
+const botviado = 'a tu que te coje tu tio😡'
+const numbotfeio = 'tu puta madre culero😡'
+// KEYS
+
+const apiTobz = 'key'
+// pra pegar a key do Tobz va até o numero 
+//wa.me/447418349954 se registre no bot
+// e depois escreva #generateapikey que o 
+//bot do tobz vai gerar sua key
+
+// MENSAGENS DE EXCLUSIVIDADE
+let mess = {
+    wait: '⌛ ESPERA NO SOY FLASH... ⌛',
+    success: '✔️ LISTO! ✔️',
+    error: {
+        stick: '❌error intenta al rato ❌',
+        Iv: '❌ Link inválido ❌'
+    },
+    only: {
+        group: '❌ Este cando es solo para grupo! ❌',
+        ownerG: '❌ Este comando solo puede ser usado por el proprietário! ❌',
+        ownerB: '❌ Este comando sólo puede ser usado por el número proprietário! ❌',
+        admin: '❌ SOLO ADMIM PUEDE USAR EL COMANDO ❌',
+        Badmin: '❌BOT REQUIERE ADMIN! ❌'
+    }
+}
+
+// CONTATO DO CRIADOR
+
+const vcard = 'BEGIN:VCARD\n' 
++ 'VERSION:3.0\n' 
++ 'FN:Meu criador^~^\n' 
++ 'ORG:Ian;\n' 
++ 'TEL;type=CELL;type=VOICE;waid=573175852207:+57 31 7585-2207\n' 
++ 'END:VCARD'
+//vcard do propietário, mude os numeros de acordo com o formato que
+//ele corresponde
+
+
+// Nem ouse mexer aqui se n quiser que de erro
+exports.qnttravahardroleta = qnttravahardroleta
+exports.blockedcmdmsg = blockedcmdmsg
+exports.blockedmsg = blockedmsg
+exports.banmsgpalavra = banmsgpalavra
+exports.adminmsgpalavra = adminmsgpalavra
+exports.apikeyporn = apikeyporn
+exports.linkbotlindo = linkbotlindo
+exports.botlindo = botlindo
+exports.botfeio = botfeio
+exports.cadebot = cadebot
+exports.botfdp = botfdp
+exports.botgostoso = botgostoso
+exports.botfofo = botfofo
+exports.botbaianor = botbaianor
+exports.botcorno = botcorno
+exports.botputa = botputa
+exports.botgay = botgay
+exports.botviado = botviado
+exports.numbotfeio = numbotfeio
+exports.linkimgday = linkimgday
+exports.textmsgday = textmsgday
+exports.textmsgnig = textmsgnig
+exports.linkimgnig = linkimgnig
+exports.textmsgeve = textmsgeve
+exports.linkimgeve = linkimgeve
+exports.adminmsgtype = adminmsgtype
+exports.adminmsgporn = adminmsgporn
+exports.adminmsglink = adminmsglink
+exports.banmsglink = banmsglink
+exports.banmsgporn = banmsgporn
+exports.banmsgtype = banmsgtype
+exports.msgerr = msgerr
+exports.mess = mess
+exports.apiTobz = apiTobz
+exports.prefix = prefix
+exports.cr = cr
+exports.vcard = vcard
+exports.OriginalOwner = OriginalOwner
+exports.ownerNumber = ownerNumber
---
+++ welcomtext.js
@@ -0,0 +1,9 @@
+const welcome = (number, groupname) => {
+    return `Joder  @${number}.Que bueno verte por aca 🥺 ${groupname} Recuerda leer las reglas para evitar mal entendidos😉`
+}
+exports.welcome = welcome
+
+const bye = (number) => {
+    return `Que bueno que te fuiste🤩 @${number}. 😤Ya no te queríamos ojala nunca regreses bye 👋`
+}
+exports.bye = bye
